d2e638a98c241a1b91d2f437f705b7774b6ad7f7
Syanmil <syanmil@yahoo.com>
2017-01-09 12:49:09 +0700

    apple tree is good enough as base

--- mango_tree_2.js
+++ mango_tree_2.js
@@ -1,8 +1,37 @@
 "use strict"
 
 // Release 0
-class AppleTree {}
-class Apple {}
+class AppleTree {
+  constructor(){
+    this.height = 20;
+    this.buah = [];
+    this.umur = 0;
+    this.maxHeight = 300;
+    this.maxUmur = 15;
+    this.Healthy = true
+  }
+  grow(){
+    if (this.umur <= this.maxUmur){
+      this.umur++;
+      if (this.height <= this.maxHeight){
+        this.height += 10;
+      }
+    } else {
+      this.Healthy = false;
+    }
+  }
+  produce(){
+    if (this.Healthy){
+      this.buah.push(new Apple);
+    }
+  }
+}
+class Apple {
+  constructor(){
+    this.quality = 'Good' || 'Bad'
+    this.size = 'kecil' || 'besar'
+  }
+}
 
 // Release 1
 class FruitTree {}
