43d0dabd55e116741b47ec7a54be69f1de903ba1
Syanmil <syanmil@yahoo.com>
2017-01-09 15:57:55 +0700

    done with tester fruit tree only

--- mango_tree_2.js
+++ mango_tree_2.js
@@ -35,16 +35,19 @@
 
 // Release 1
 class FruitTree {
-  constructor(){
-    this.umur = 0;
+  constructor(umur, height, jmlbuah, healthy){
+    this.umur = umur || 0;
+    this.pohon = "Pohon Buah"
     this.maxUmur = 100;
     this.umurDewasa = 20;
     this.maxProduce = 20;
-    this.healthy = true;
-    this.height = 0;
-    this.heightModifier = 10
+    this.healthy = healthy || true;
+    this.height = height || 0;
+    this.heightModifier = 10;
     this.maxHeight = 50;
-    this.buah = []
+    this.buah = [];
+    this.jmlbuah = jmlbuah || 0;
+    this.fruit = new Fruit()
   }
   grow(){
     if (this.maxUmur >= this.umur){
@@ -56,17 +59,14 @@ class FruitTree {
       this.healthy = false
     }
   }
-
   produce(){
-    if (this.healthy){
+    this.buah = []
+    if (this.healthy && this.umur >= this.umurDewasa){
       for (let i = 0; i < Math.floor(Math.random()*this.maxProduce); i++){
-        this.buah.push(new Fruit)
+        this.buah.push(this.fruit)
       }
+      this.jmlbuah = this.buah.length
     }
-  }
-
-  harvest(){
-
   }
 }
 
@@ -78,5 +78,110 @@ class Fruit {
   }
 }
 
+class MangoTree extends FruitTree{
+  constructor(umur, height, maxUmur, umurDewasa, maxProduce, healthy, heightModifier, maxHeight, buah){
+    super(umur, height, maxUmur, umurDewasa, maxProduce, healthy, heightModifier, maxHeight, buah)
+  }
+  grow(){
+    super.grow()
+  }
+  produce(){
+    super.produce()
+  }
+}
+
+class AppleTree extends FruitTree{
+  constructor(umur, height, maxUmur, umurDewasa, maxProduce, healthy, heightModifier, maxHeight, buah){
+    super(umur, height, maxUmur, umurDewasa, maxProduce, healthy, heightModifier, maxHeight, buah)
+  }
+  grow(){
+    super.grow()
+  }
+  produce(){
+    super.produce()
+  }
+}
+
+class PearTree extends FruitTree{
+  constructor(umur, height, maxUmur, umurDewasa, maxProduce, healthy, heightModifier, maxHeight, buah){
+    super(umur, height, maxUmur, umurDewasa, maxProduce, healthy, heightModifier, maxHeight, buah)
+  }
+  grow(){
+    super.grow()
+  }
+  produce(){
+    super.produce()
+  }
+}
+
+// var buah = new FruitTree(5, 7)
+// console.log(buah.fruit);
 // Release 2
-class TreeGrove {}
+class TreeGrove {
+  constructor(){
+    this.treeColection = []
+  }
+  inputTree(treeClass, umur, tinggi, jmlbuah, sehat){
+    let newTree
+    switch (treeClass) {
+      case MangoTree:
+      newTree = new MangoTree(umur, tinggi, jmlbuah, sehat)
+        break;
+      case AppleTree:
+      newTree = new AppleTree(umur, tinggi, jmlbuah, sehat)
+        break;
+      case PearTree:
+      newTree = new PearTree(umur, tinggi, jmlbuah, sehat)
+        break;
+      default:
+      newTree = new FruitTree(umur, tinggi, jmlbuah, sehat)
+    }
+    this.treeColection.push(newTree)
+  }
+  showAge(){
+    this.treeColection.forEach(function(val){
+      console.log(`${val.pohon} berumur ${val.umur}`);
+    })
+  }
+  showTree(){
+    this.treeColection.forEach(function(val){
+      console.log(val);
+    })
+  }
+  matureTrees(){
+    this.treeColection.forEach(function(val){
+      if (val.jmlbuah > 0){
+        console.log(`${val.pohon} berbuah ${val.jmlbuah} biji`);
+      }
+    })
+  }
+  nextYear(){
+    this.treeColection.forEach(function(val){
+      val.grow()
+      val.produce()
+    })
+  }
+  deadTrees(){
+    this.treeColection.forEach(function(val){
+      if (val.healthy == false){
+        console.log(`${val.pohon} telah mati pada umur ${val.umur}`);
+      }
+    })
+  }
+}
+var buah = new FruitTree()
+buah.grow();
+buah.grow();
+// console.log(buah);
+
+var grove = new TreeGrove()
+grove.inputTree("FruitTree", 3, 5, 12, true)
+grove.inputTree("FruitTree", 7, 3, 5, true)
+grove.inputTree("FruitTree", 2, 7, 2, true)
+grove.inputTree("FruitTree", 4, 4, 9, true)
+grove.showAge()
+grove.showTree()
+grove.matureTrees()
+grove.nextYear()
+grove.deadTrees()
+// console.log(grove);
